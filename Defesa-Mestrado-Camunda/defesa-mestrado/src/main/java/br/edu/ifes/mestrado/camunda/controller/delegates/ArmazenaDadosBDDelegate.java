81e6d54ab3063490bf14d6cb4d05a39c81eb242c
Davidsonnj <davidsoncs45@gmail.com>
2025-04-09 13:33:52 -0300

    docs: BPMNs em um pasta separada

---
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/ArmazenaDadosBDDelegate.java
@@ -0,0 +1,11 @@
+package br.edu.ifes.mestrado.camunda.controller.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class ArmazenaDadosBDDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+    }
+}
