88305071e555386a2f8284287405aa3fb144472f
Davidsonnj <davidsoncs45@gmail.com>
2025-04-10 16:08:33 -0300

    feat: implementa o models de banca

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/model/Banca.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/model/Banca.java
@@ -1,16 +1,31 @@
 package br.edu.ifes.mestrado.camunda.model;
 
 public class Banca {
+    private int idBanca;
     private String nome;
     private String email;
     private String instituicao;
     private String minicurriculo;
+
+    /**
+     * Constrói um objeto Banca com as informações fornecidas.
+     *
+     * @param nome O nome do membro da banca.
+     * @param email O e-mail do membro da banca.
+     * @param instituicao A instituição onde o membro da banca trabalha.
+     * @param minicurriculo Um resumo breve (minicurrículo) sobre o membro da banca.
+     *
+     **/
 
     public Banca(String nome, String email, String instituicao, String minicurriculo) {
         this.nome = nome;
         this.email = email;
         this.instituicao = instituicao;
         this.minicurriculo = minicurriculo;
+    }
+
+    public int getIdBanca() {
+        return idBanca;
     }
 
     public String getNome() {
@@ -27,6 +42,10 @@ public class Banca {
 
     public String getMinicurriculo() {
         return minicurriculo;
+    }
+
+    public void setIdBanca(int idBanca) {
+        this.idBanca = idBanca;
     }
 
     public void setNome(String nome) {
