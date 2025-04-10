65ce00d2b46da2f4b65c95151d16095557b1f4e2
Davidsonnj <davidsoncs45@gmail.com>
2025-04-10 16:09:59 -0300

    feat: implementa tratamento de excecao

---
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/exception/ErroInsercaoBancoException.java
@@ -0,0 +1,7 @@
+package br.edu.ifes.mestrado.camunda.exception;
+
+public class ErroInsercaoBancoException extends RuntimeException {
+    public ErroInsercaoBancoException(String message) {
+        super(message);
+    }
+}
