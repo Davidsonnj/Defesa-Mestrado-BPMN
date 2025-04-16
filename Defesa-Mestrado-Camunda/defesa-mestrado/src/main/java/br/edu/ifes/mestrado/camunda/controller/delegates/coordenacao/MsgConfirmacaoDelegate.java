cd59ae817737d17b4190cd89db9a62d8b5a2d9ad
Davidsonnj <davidsoncs45@gmail.com>
2025-04-16 16:33:50 -0300

    feat: implementa o metodo de enviar a mensagem de anuencia da coordenacao para o sistema principal

---
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/coordenacao/MsgConfirmacaoDelegate.java
@@ -0,0 +1,43 @@
+package br.edu.ifes.mestrado.camunda.controller.delegates.coordenacao;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MsgConfirmacaoDelegate implements JavaDelegate {
+
+    public void execute(DelegateExecution execution) throws Exception {
+        RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        String businessKey = execution.getProcessBusinessKey();
+
+        long count = runtimeService.createExecutionQuery()
+                .processInstanceBusinessKey(businessKey)
+                .messageEventSubscriptionName("MsgCoordenacao")
+                .count();
+        System.out.println("Bussiness Key: " + businessKey + " Envio de resposta da anuência");
+        if (count > 0){
+            Boolean anuencia = (Boolean) execution.getVariable("anuencia");
+            String justificativaAnuencia;
+
+            if (execution.hasVariable("justificativaAnuencia")){
+                justificativaAnuencia = (String) execution.getVariable("justificativaAnuencia");
+                execution.setVariable("justificativaAnuencia", anuencia);
+
+            }else {
+                justificativaAnuencia = "Não foi informada nenhuma justificativa.";
+            }
+
+            runtimeService.createMessageCorrelation("MsgCoordenacao")
+                    .setVariable("nome", execution.getVariable("nome"))
+                    .processInstanceBusinessKey(businessKey)
+                    .correlate();
+        } else {
+            System.out.println("Nenhuma instância encontrada esperando pela mensagem Envio de dados do aluno.");
+        }
+
+    }
+
+}
