9ec5f444a488c3d4a26262e3c91ebff8a551438f
Davidsonnj <davidsoncs45@gmail.com>
2025-04-17 15:06:04 -0300

    feat: envia mensagem de defesa negada para processos dependentes

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/EnviarJustificativaAnuenciaDelegate.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/EnviarJustificativaAnuenciaDelegate.java
@@ -1,5 +1,6 @@
 package br.edu.ifes.mestrado.camunda.controller.delegates.SistemaPrincipal;
 
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -7,6 +8,26 @@ public class EnviarJustificativaAnuenciaDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution){
         System.out.println("Chegou em 'Jusitifcativa de defesa negada reportada' no BPMN");
+
+        RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        String businessKey = execution.getProcessBusinessKey();
+
+        long count = runtimeService.createExecutionQuery()
+                .processInstanceBusinessKey(businessKey)
+                .messageEventSubscriptionName("DefesaNegada")
+                .count();
+
+        System.out.println("Bussiness Key: " + businessKey + " Envio de justificativa da anuência");
+        if (count > 0){
+            String justificativaAnuencia = (String) execution.getVariable("justificativaAnuencia");
+
+            runtimeService.createMessageCorrelation("DefesaNegada")
+                    .setVariable("justificativaAnuencia", justificativaAnuencia)
+                    .processInstanceBusinessKey(businessKey)
+                    .correlate();
+        } else {
+            System.out.println("Nenhuma instância encontrada esperando pela mensagem Envio de justificativa no sistema principal.");
+        }
 
     }
 }
