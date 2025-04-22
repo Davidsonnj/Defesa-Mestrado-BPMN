cec0ac06241919916a8f2c4eca73e58caffff35a
Davidsonnj <davidsoncs45@gmail.com>
2025-04-22 15:25:04 -0300

    fix: Corrige texto do body

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/coordenacao/EnviarEmailSolicitacaoAnuenciaDelegate.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/coordenacao/EnviarEmailSolicitacaoAnuenciaDelegate.java
@@ -16,8 +16,8 @@ public class EnviarEmailSolicitacaoAnuenciaDelegate implements JavaDelegate {
         String subject = "Solicitação de Anuência do aluno " + aluno ;
         String body = "Prezado(a) Coordenador(a),\n\n"
                 + "Estamos entrando em contato para solicitar a anuência do " + aluno + " que tem a defesa intitulada de '"+ titulo_trabalho +"', favor seguir exatamente essa formatação, sem nenhuma adição de informações:\n\n"
-                + "O assunto deve ser: Anuencia de defesa - 'Titutlo da defesa'"
-                + "Caso tenha sido negado enviar a mensagem com a seguinta formatação: 'Jutificativa: (Colocar o motivo da rejeição)'"
+                + "O assunto deve ser: Anuencia de defesa - 'Titutlo da defesa' "
+                + "Caso tenha sido negado enviar a mensagem com a seguinta formatação: 'Jutisficativa: (Colocar o motivo da rejeição)'"
                 + "Caso seja autorizado enviar no corpo da mensagem somente a palavra Autorizado\n\n"
                 + "Por favor, preencha essas informações o mais breve possível para darmos continuidade ao processo.\n\n"
                 + "Atenciosamente,\n"
