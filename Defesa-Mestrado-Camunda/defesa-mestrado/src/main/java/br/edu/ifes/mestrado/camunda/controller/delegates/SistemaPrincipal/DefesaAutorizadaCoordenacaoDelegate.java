a08c532ba2cb8b52d2eff4083bfacfbd4e5c3790
Davidsonnj <davidsoncs45@gmail.com>
2025-04-23 16:11:14 -0300

    feat: associacao dos bpmns para defesa autorizada

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/DefesaAutorizadaCoordenacaoDelegate.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/DefesaAutorizadaCoordenacaoDelegate.java
@@ -1,5 +1,6 @@
 package br.edu.ifes.mestrado.camunda.controller.delegates.SistemaPrincipal;
 
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -7,8 +8,19 @@ public class DefesaAutorizadaCoordenacaoDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution){
+        String businessKey = execution.getProcessBusinessKey();
 
-        System.out.println("Chegou em 'Defesa Autorizada' no BPMN");
+        RuntimeService runtimeService =  execution.getProcessEngineServices().getRuntimeService();
 
+        long count = runtimeService.createExecutionQuery()
+                .processInstanceBusinessKey(businessKey)
+                .messageEventSubscriptionName("DefesaAutorizada")
+                .count();
+        if (count > 0) {
+            runtimeService.createMessageCorrelation("DefesaAutorizada")
+                    .processInstanceBusinessKey(businessKey)
+                    .correlate();
+
+        }
     }
 }
