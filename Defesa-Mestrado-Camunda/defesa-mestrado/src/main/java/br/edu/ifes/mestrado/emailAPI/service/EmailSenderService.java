27d77539c393f6a49b8d56150285496b307266f7
Davidsonnj <davidsoncs45@gmail.com>
2025-04-24 12:56:49 -0300

    feat: adiciona metodo de enviar arquivos nos emails

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/emailAPI/service/EmailSenderService.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/emailAPI/service/EmailSenderService.java
@@ -2,7 +2,10 @@ package br.edu.ifes.mestrado.emailAPI.service;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.List;
 import java.util.Properties;
 
 public class EmailSenderService {
@@ -47,4 +50,46 @@ public class EmailSenderService {
             return false;
         }
     }
+    public boolean sendEmail(String to, String subject, String body, List<String> attachmentPaths) {
+        try {
+            Properties properties = new Properties();
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.host", host);
+            properties.put("mail.smtp.port", "587");
+            properties.put("mail.smtp.ssl.trust", host);
+
+            Session session = Session.getInstance(properties, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(body);
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+
+            for (String filePath : attachmentPaths) {
+                MimeBodyPart attachmentPart = new MimeBodyPart();
+                attachmentPart.attachFile(filePath);
+                multipart.addBodyPart(attachmentPart);
+            }
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
