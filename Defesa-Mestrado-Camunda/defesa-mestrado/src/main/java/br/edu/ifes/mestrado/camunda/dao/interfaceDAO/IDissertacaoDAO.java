9056b20211a3c64eef2c5d49aa2b25d48436626a
Davidsonnj <davidsoncs45@gmail.com>
2025-04-24 12:59:31 -0300

    feat: interface sobre dissertações no banco

---
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/dao/interfaceDAO/IDissertacaoDAO.java
@@ -0,0 +1,7 @@
+package br.edu.ifes.mestrado.camunda.dao.interfaceDAO;
+
+import br.edu.ifes.mestrado.camunda.model.Defesa;
+
+public interface IDissertacaoDAO {
+    int inserir(int idDefesa, String urls);
+}
