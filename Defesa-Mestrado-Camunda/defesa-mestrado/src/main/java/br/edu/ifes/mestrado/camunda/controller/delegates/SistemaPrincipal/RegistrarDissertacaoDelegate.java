11d4f3362f0a5a559374947312c75f9e80ebca69
Davidsonnj <davidsoncs45@gmail.com>
2025-04-24 13:01:24 -0300

    feat: metodo para registrar dissertação do camunda para o banco

---
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/RegistrarDissertacaoDelegate.java
@@ -0,0 +1,16 @@
+package br.edu.ifes.mestrado.camunda.controller.delegates.SistemaPrincipal;
+
+import br.edu.ifes.mestrado.camunda.dao.implementations.DissertacaoDAO;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class RegistrarDissertacaoDelegate implements JavaDelegate {
+    public void execute(DelegateExecution execution) throws Exception {
+        int idDefesa = (int) execution.getVariable("idDefesaBD");
+        String caminhosDosAnexos = (String) execution.getVariable("caminhosDosAnexos");
+
+        DissertacaoDAO dissertacaoDAO = new DissertacaoDAO();
+        dissertacaoDAO.inserir(idDefesa, caminhosDosAnexos);
+        System.out.println("Dissertacao registrada com sucesso!");
+    }
+}
