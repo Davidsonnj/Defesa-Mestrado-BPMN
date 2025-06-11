fd40ed86981da6ca15a03220ce1e308a07b28146
Davidsonnj <davidsoncs45@gmail.com>
2025-06-11 15:33:43 -0300

    feat: adiciona campo status e setters para controlar o estado do email

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/emailAPI/model/Email.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/emailAPI/model/Email.java
@@ -10,7 +10,47 @@ public class Email {
     private Date date;
     private String body;
     private List<String> attachmentPaths;
+    private String status;
 
+    public Email(){}
+
+    public long getUid() { return uid; }
+    public List<String> getAttachmentPaths() {
+        return attachmentPaths;
+    }
+    public String getSubject() { return subject; }
+    public String getSender() { return sender; }
+    public Date getDate() { return date; }
+    public String getBody() { return body; }
+    public String getStatus() { return status; }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setAttachmentPaths(List<String> attachmentPaths) {
+        this.attachmentPaths = attachmentPaths;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public Email(long uid, String subject, String sender, Date date, String body, List<String> attachmentPaths) {
         this.uid = uid;
@@ -29,16 +69,5 @@ public class Email {
         this.body = body;
     }
 
-    public long getUid() { return uid; }
-    public List<String> getAttachmentPaths() {
-        return attachmentPaths;
-    }
-
-    public void setAttachmentPaths(List<String> attachmentPaths) {
-        this.attachmentPaths = attachmentPaths;
-    }
-    public String getSubject() { return subject; }
-    public String getSender() { return sender; }
-    public Date getDate() { return date; }
-    public String getBody() { return body; }
 }
+
