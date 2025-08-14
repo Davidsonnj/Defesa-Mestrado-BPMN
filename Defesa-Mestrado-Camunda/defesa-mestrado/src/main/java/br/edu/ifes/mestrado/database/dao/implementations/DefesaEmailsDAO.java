a43d6866a008188c96ad534412f38ae70ed6b5b0
Davidsonnj <davidsoncs45@gmail.com>
2025-08-14 20:20:54 -0300

    feat: adiciona a insercao da relacao entre defesa e email

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/database/dao/implementations/DefesaEmailsDAO.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/database/dao/implementations/DefesaEmailsDAO.java
@@ -1,4 +1,37 @@
 package br.edu.ifes.mestrado.database.dao.implementations;
 
-public class DefesaEmailsDAO {
+import br.edu.ifes.mestrado.database.DatabaseConnection;
+import br.edu.ifes.mestrado.database.dao.interfaceDAO.IDefesaBancaDAO;
+import br.edu.ifes.mestrado.database.dao.interfaceDAO.IDefesaEmailDAO;
+
+import java.sql.*;
+
+public class DefesaEmailsDAO implements IDefesaEmailDAO {
+
+    @Override
+    public int inserir(int idDefesa, long idEmail) throws SQLException {
+        String sql = "INSERT INTO Defesa_Banca (idDefesa, idEmail) VALUES (?, ?)";
+        try {
+            Connection connection = DatabaseConnection.getInstance();
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setInt(1, idDefesa);
+            stmt.setLong(2, idEmail);
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                int idGerado = rs.getInt(1);
+                System.out.println("Email salvo com sucesso! ID: " + idGerado);
+                return idGerado;
+            }
+            System.out.println("Email salvo com sucesso! Id nao retornou.");
+            return -1;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/database/dao/interfaceDAO/IDefesaEmailDAO.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/database/dao/interfaceDAO/IDefesaEmailDAO.java
@@ -1,6 +1,8 @@
 package br.edu.ifes.mestrado.database.dao.interfaceDAO;
 
+import java.sql.SQLException;
+
 public interface IDefesaEmailDAO {
-    int  inserir(int idDefesa, long idEmail);
+    int  inserir(int idDefesa, long idEmail) throws SQLException;
 
 }
