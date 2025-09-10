cb224f725dbd52103397c549fea8225c95155b0f
Davidsonnj <davidsoncs45@gmail.com>
2025-09-10 15:05:16 -0300

    feat: gerador do corpo da msh

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/EnviarDissertaçãoBancaDelegate.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/camunda/controller/delegates/SistemaPrincipal/EnviarDissertaçãoBancaDelegate.java
@@ -1,6 +1,7 @@
 package br.edu.ifes.mestrado.camunda.controller.delegates.SistemaPrincipal;
 
 import br.edu.ifes.mestrado.camunda.model.Banca;
+import br.edu.ifes.mestrado.camunda.model.Orientador;
 import br.edu.ifes.mestrado.emailAPI.controller.SenderEmailController;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -23,6 +24,9 @@ public class EnviarDissertaçãoBancaDelegate implements JavaDelegate {
         String aluno = (String) execution.getVariable("aluno");
         String titulo_trabalho = (String) execution.getVariable("titulo_trabalho");
         String caminhosDosAnexos = (String) execution.getVariable("caminhosDosAnexos");
+        String emailOrientador  = (String) execution.getVariable("emailOrientador");
+        String nomeCoorientador = (String) execution.getVariable("nomeCoorientador");
+        String emailCoorientador = (String) execution.getVariable("emailCoorientador");
 
         List<String> paths = (caminhosDosAnexos != null && !caminhosDosAnexos.isBlank())
                 ? Arrays.asList(caminhosDosAnexos.split(";"))
@@ -30,19 +34,32 @@ public class EnviarDissertaçãoBancaDelegate implements JavaDelegate {
 
         List<Banca> bancaList = (List<Banca>) execution.getVariable("bancaDefesa");
 
+        String subject = "Dissertação do(a) aluno(a): " + aluno;
+
+        String bodyOrientador = gerarCorpoEmail("Orientador(a)", titulo_trabalho);
+        emailSender.sendEmail(emailOrientador, subject, bodyOrientador, paths);
+
+        if (nomeCoorientador != null && !nomeCoorientador.isBlank() &&
+                emailCoorientador != null && !emailCoorientador.isBlank()) {
+
+            String bodyCoorientador = gerarCorpoEmail(nomeCoorientador,titulo_trabalho);
+            emailSender.sendEmail(emailCoorientador, subject, bodyCoorientador, paths);
+
+        }
+
         for (Banca banca : bancaList) {
-            String subject = "Dissertação do(a) aluno(a): " + aluno;
-            String body = "Prezado(a) " + banca.getNome() + ",<br><br>" +
-                    "Informamos que a dissertação intitulada &quot;" + titulo_trabalho + "&quot; está anexada a este e-mail para sua apreciação.<br><br>" +
-                    "Caso haja qualquer dúvida ou necessidade de informações adicionais, por favor, não hesite em nos contatar.<br><br>" +
-                    "Atenciosamente,<br><br>" +
-                    "Programa de Pós-Graduação em Computação Aplicada (PPComp)<br>" +
-                    "IFES – Campus Serra";
-
-
+            String body = gerarCorpoEmail(banca.getNome(), titulo_trabalho);
 
             emailSender.sendEmail(banca.getEmail(), subject, body, paths);
         }
         LOGGER.info("Notificou todos os integrantes da banca!");
+    }
+    private String gerarCorpoEmail(String nomeDestinatario, String titulo_trabalho) {
+        return "Prezado(a) " + nomeDestinatario + ",<br><br>" +
+                "Informamos que a dissertação intitulada &quot;" + titulo_trabalho + "&quot; está anexada a este e-mail para sua apreciação.<br><br>" +
+                "Caso haja qualquer dúvida ou necessidade de informações adicionais, por favor, não hesite em nos contatar.<br><br>" +
+                "Atenciosamente,<br><br>" +
+                "Programa de Pós-Graduação em Computação Aplicada (PPComp)<br>" +
+                "IFES – Campus Serra";
     }
 }
