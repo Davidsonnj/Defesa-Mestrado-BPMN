0ff65cd25d3abd38345f77a32c0c6f459eed77bb
Davidsonnj <davidsoncs45@gmail.com>
2025-10-09 14:36:53 -0300

    feat: corrige erro de exposição de chave da api do gemini

--- Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/GenAI/GeminiAPI.java
+++ Defesa-Mestrado-Camunda/defesa-mestrado/src/main/java/br/edu/ifes/mestrado/GenAI/GeminiAPI.java
@@ -1,26 +1,48 @@
 package br.edu.ifes.mestrado.GenAI;
 
 import com.google.genai.Client;
+import com.google.genai.errors.ServerException;
 import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.HttpOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class GeminiAPI {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String apiKey = dotenv.get("GEMINI_API_KEY");
+
     private static final Client client = Client.builder()
-            .apiKey("AIzaSyBu_ZZ3dO_qPL-cBr83dnBGB3YJafHHYXw")
+            .apiKey(apiKey)
+            .httpOptions(HttpOptions.builder().apiVersion("v1").build())
             .build();
 
-    public String perguntar(String pergunta) throws Exception{
-        try {
-            GenerateContentResponse response = client.models.generateContent(
-                    "gemini-1.5-flash",
-                    pergunta,
-                    null
-            );
-            return response.text();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
+    public String perguntar(String pergunta) throws Exception {
+        int retries = 0;
+        int maxRetries = 2;
+        long waitTime = 5000;
+
+        while (true) {
+            try {
+                GenerateContentResponse response = client.models.generateContent(
+                        "gemini-2.0-flash",
+                        pergunta,
+                        null
+                );
+                return response.text();
+
+            } catch (ServerException e) {
+                if (retries >= maxRetries) {
+                    throw new RuntimeException("Falha após várias tentativas: " + e.getMessage(), e);
+                }
+                retries++;
+                System.out.println("Erro 503, tentativa " + retries + " de " + maxRetries + ". Retentando em " + waitTime + "ms");
+                Thread.sleep(waitTime);
+                waitTime *= 2;
+
+            } catch (Exception e) {
+                throw e;
+            }
         }
     }
 }
